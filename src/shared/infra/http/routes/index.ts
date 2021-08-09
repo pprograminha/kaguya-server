285793da81c83dcaa44d1fe217ecd4df6f09b8a9
Tiago Gonçalves <tiaguin180@gmail.com>
2021-08-09 15:32:20 -0300

    feat: Using session router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,13 @@
 import { usersRouter } from '@modules/users/infra/http/routes/users.routes';
 import { platformRolesRouter } from '@modules/platformRoles/infra/http/routes/platformRoles.routes';
 import { Router } from 'express';
+import { sessionRouter } from '@modules/users/infra/http/routes/session.routes';
 
 const router = Router();
 
 router.use('/users', usersRouter);
+router.use('/session', sessionRouter);
+
 router.use('/platform/roles', platformRolesRouter);
 
 export { router };

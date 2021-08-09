8e48aa4fa2fd195abb60bff33a6441021410be09
Tiago Gonçalves <tiaguin180@gmail.com>
2021-08-08 23:55:55 -0300

    feat: Using platform roles router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { usersRouter } from '@modules/users/infra/http/routes/users.routes';
+import { platformRolesRouter } from '@modules/platformRoles/infra/http/routes/platformRoles.routes';
 import { Router } from 'express';
 
 const router = Router();
 
 router.use('/users', usersRouter);
+router.use('/platform/roles', platformRolesRouter);
 
 export { router };
